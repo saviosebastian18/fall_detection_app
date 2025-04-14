a47092c59f6400b585ea72eae294a53a358eb5de
saviosebastian18 <165919206+saviosebastian18@users.noreply.github.com>
2025-04-14 10:29:01 +0530

    Delete get_supabase_token.py

--- get_supabase_token.py
+++
@@ -1,5 +0,0 @@
-import os
-
-API_KEY = os.getenv("AIzaSyBCa6-B8L0isD81EpBzkHqVa674SCQD8vk")  # Ensure you're loading it correctly
-
-print("API KEY:", API_KEY)  # Debugging: See if the key is loaded correctly
